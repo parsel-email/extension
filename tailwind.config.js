2e34f27977e4389f57493f5e7bc9f5ac0dc71661
doorknob88 <git@doorknob88.me>
2025-05-17 00:11:51 -0700

    Add Tailwind CSS plugins and update configuration; remove old Tailwind config file

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  mode: "jit",
-  darkMode: "class",
-  content: ["./src/**/*.{js,jsx,ts,tsx,html}"],
-  plugins: [
-    require("daisyui")
-  ],
-}
---
+++ tailwind.config.ts
@@ -0,0 +1,52 @@
+import containerQueries from '@tailwindcss/container-queries';
+import forms from '@tailwindcss/forms';
+import typography from '@tailwindcss/typography';
+import daisyui from 'daisyui';
+import type { Config } from 'tailwindcss';
+
+export default {
+  content: ['./src/**/*.{html,js,ts}'],
+
+  daisyui: {
+    themes: [
+      "light",
+      "dark",
+      "cupcake",
+      "bumblebee",
+      "emerald",
+      "corporate",
+      "synthwave",
+      "retro",
+      "cyberpunk",
+      "valentine",
+      "halloween",
+      "garden",
+      "forest",
+      "aqua",
+      "lofi",
+      "pastel",
+      "fantasy",
+      "wireframe",
+      "black",
+      "luxury",
+      "dracula",
+      "cmyk",
+      "autumn",
+      "business",
+      "acid",
+      "lemonade",
+      "night",
+      "coffee",
+      "winter",
+      "dim",
+      "nord",
+      "sunset",
+    ],
+  },
+
+  theme: {
+    extend: {}
+  },
+
+  plugins: [typography, forms, containerQueries, daisyui]
+} satisfies Config;
